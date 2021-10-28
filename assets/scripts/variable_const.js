e4238fd84e13cb6d0781e8a4f43bf8d576eb8fd1
Alina Kozlova <Art-line.lina@yandex.com>
2021-10-28 18:17:06 +0300

    1

--- assets/scripts/variable_const.js
+++ assets/scripts/variable_const.js
@@ -10,8 +10,12 @@ let currentResult = defaultResult;
 
 currentResult = (currentResult + 10) * 3 / 2 - 1;
 
-let calculationDescription = '(' + defaultResult + ' + 10) * 3 / 2 - 1';
+// let calculationDescription = '(' + defaultResult + ' + 10) * 3 / 2 - 1';
 // let calculationDescription = '('+ currentResult + ' + 10) * 3 / 2 - 1';
-let calculationDescription = 'Hi there';
+// let calculationDescription = 'Hi there';
+let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
+// let errorMessge = "Ann error" +
+//                     "occurred";
 
 outputResult(currentResult, calculationDescription);
+// outputResult(currentResult, errorMessge);
