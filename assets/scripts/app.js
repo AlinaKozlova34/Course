7fa6d1a3a1833f15c105fd8d6e02beff124a4401
Alina Kozlova <Art-line.lina@yandex.com>
2021-10-28 13:51:53 +0300

    var

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,34 +1,46 @@
-const backdropElement = document.getElementById('backdrop');
-const modalLinkElements = document.querySelectorAll('.info-modal');
-let infoModal;
+// const backdropElement = document.getElementById('backdrop');
+// const modalLinkElements = document.querySelectorAll('.info-modal');
+// let infoModal;
 
-function toggleBackdrop() {
-  backdropElement.classList.toggle('visible');
-}
+// function toggleBackdrop() {
+//   backdropElement.classList.toggle('visible');
+// }
 
-function presentInfoModal(event) {
-  const text = event.target.dataset.text;
-  toggleBackdrop();
-  infoModal = document.createElement('div');
-  infoModal.classList.add('modal');
-  infoModal.innerHTML = `
-    <h2>More Details</h2>
-    <p>${text}</p>
-  `;
-  const closeButton = document.createElement('button');
-  closeButton.addEventListener('click', hideInfoModal);
-  closeButton.textContent = 'Okay';
-  infoModal.appendChild(closeButton);
-  document.body.appendChild(infoModal);
-}
+// function presentInfoModal(event) {
+//   const text = event.target.dataset.text;
+//   toggleBackdrop();
+//   infoModal = document.createElement('div');
+//   infoModal.classList.add('modal');
+//   infoModal.innerHTML = `
+//     <h2>More Details</h2>
+//     <p>${text}</p>
+//   `;
+//   const closeButton = document.createElement('button');
+//   closeButton.addEventListener('click', hideInfoModal);
+//   closeButton.textContent = 'Okay';
+//   infoModal.appendChild(closeButton);
+//   document.body.appendChild(infoModal);
+// }
 
-function hideInfoModal() {
-  toggleBackdrop();
-  document.body.removeChild(infoModal);
-}
+// function hideInfoModal() {
+//   toggleBackdrop();
+//   document.body.removeChild(infoModal);
+// }
 
-for (const linkElement of modalLinkElements) {
-  linkElement.addEventListener('click', presentInfoModal);
-}
+// for (const linkElement of modalLinkElements) {
+//   linkElement.addEventListener('click', presentInfoModal);
+// }
 
-backdropElement.addEventListener('click', hideInfoModal);
+// backdropElement.addEventListener('click', hideInfoModal);
+
+
+let age = 30;
+alert(age);
+
+alert(40);
+
+
+let currentResult = 0;
+ currentResult = (currentResult + 10) * 3/2 - 1;
+
+ otputResult(currentResult, '');
